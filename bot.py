830e32e00ad8926d7f2bc1adcc3c927b56501bdd
Roman Tyurin <tyurroma@yandex.ru>
2018-09-13 17:55:02 +0300

    Added planet fucntion

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import settings
+import ephem
+import datetime as d
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -8,9 +10,46 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
                     )
 
 def greet_user(bot, update):
-    text = 'Called /start'
+    text = 'Hello, {}! With the help of this bot, you can find out' \
+            'in which constellation the planet is today. ' \
+            'Just type the command: /planet <planet name>. ' \
+            'Something like this: /planet Mars. ' \
+            'The list of planets are: Mars, Neptune, Pluto, Saturn, Uranus, Venus. '\
+            'Bot can also show what you wrote.' \
+            .format(update.message.chat.first_name)
     logging.info(text)
     update.message.reply_text(text)
+    update.message.reply_text(instruction)
+
+def planet(bot, update, args):
+    planets = ('Mars', 'Neptune', 'Pluto', 'Saturn', 'Uranus', 'Venus')
+    date_today = d.datetime.today()
+    planet_name = ' '.join(args)
+    logging.info(planet_name)
+    try: 
+        if planet_name == 'Mars':
+            planet_name = ephem.Mars(date_today)
+            update.message.reply_text(ephem.constellation(planet_name)[1])
+        elif planet_name == 'Neptune':
+            planet_name = ephem.Neptune(date_today)
+            update.message.reply_text(ephem.constellation(planet_name)[1])    
+        elif planet_name == 'Pluto':
+            planet_name = ephem.Pluto(date_today)
+            update.message.reply_text(ephem.constellation(planet_name)[1])
+        elif planet_name == 'Saturn':
+            planet_name = ephem.Saturn(date_today)
+            update.message.reply_text(ephem.constellation(planet_name)[1])
+        elif planet_name == 'Uranus':
+            planet_name = ephem.Uranus(date_today)
+            update.message.reply_text(ephem.constellation(planet_name)[1])
+        elif planet_name == 'Venus':
+            planet_name = ephem.Venus(date_today)
+            update.message.reply_text(ephem.constellation(planet_name)[1])
+        else:
+            update.message.reply_text('No data')
+    except IndexError:
+        update.message.reply_text('Enter planet name please! For example, /planet Mars')
+        logging.info ('User did not enter a planet')
 
 def talk_to_me(bot, update):
     user_text = 'Hello {}! You wrote: {}.'.format(update.message.chat.first_name, update.message.text)
@@ -23,12 +62,10 @@ def main():
     logging.info('Bot is starting')
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler('planet', planet, pass_args=True))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     mybot.start_polling()
     mybot.idle()
 
 if __name__ == '__main__':
     main()
-
-
-
